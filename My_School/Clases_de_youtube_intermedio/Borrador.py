a0a3053267f61f07a2c8c5154bd6a92fff069290
Jesus Gomez <72950323+jesusgome09@users.noreply.github.com>
2023-06-24 05:12:23 +0000

    Good Nights

--- My_School/Clases_de_youtube_intermedio/Borrador.py
+++ My_School/Clases_de_youtube_intermedio/Borrador.py
@@ -1,4 +1,26 @@
-txt_file = open("My_School/Clases_de_youtube_intermedio/My_file.txt", "r+")
-txt_file.write(txt_file + "Hola mundo")
-print(txt_file.read())
 
+def escribir(texto):
+    with open("My_School/Clases_de_youtube_intermedio/My_file.txt", "a") as file:
+        file.write("\n"+texto)
+
+def leer():
+    with open("My_School/Clases_de_youtube_intermedio/My_file.txt", "r") as file:
+        print(file.read())
+
+
+def ingreso_datos(nombre, edad:int):
+    escribir(f"Nombre: {nombre}; Edad: {edad}")
+
+#ingreso_datos("pedro", 43)
+#leer()
+
+def buscar(nombre):
+    with open("My_School/Clases_de_youtube_intermedio/My_file.txt", "r") as file:
+        i = 0
+        e = 0
+        for line in file:
+            if nombre in line:
+                print(f"Linea: {i}; {line}")
+                 
+
+buscar("juan")
