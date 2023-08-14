519032edd4e8e54b7ed899a729535190af674b0b
jesusgome09 <1234prefierojesus@gmail.com>
2023-08-14 18:06:31 -0500

    nada

--- Splash_Car/Almacen.py
+++ Splash_Car/Almacen.py
@@ -1,17 +1,1 @@
-import streamlit as st
 
-st.write("""
-# Mi pagina web
-Bienvenido a mi pagina web
-""")
-
-ped = st.radio("¿Que desea hacer?", ("Ingresar", "Registrar"))
-file = st.file_uploader("Subir archivo", type=["txt"])
-
-if file is not None:
-    st.write(file)
-    file.seek(0)
-    content = file.read()
-    st.write(content)
-
-#   streamlit run Splash_Car\Almacen.py
